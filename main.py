2ffdb7830b7e934c4b9f686587f8b4620d304771
sereyoudomlong <sereyoudom78@gmail.com>
2026-03-19 11:44:21 +1100

    created circle shape

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
     while True:
         log_state()
+        #clock for fps
+        clock = pygame.time.Clock()
+
+        # inf loop for event handle
         for event in pygame.event.get():
+            # quit when press x on the window
             if event.type == pygame.QUIT:
                 return
+            
+            # pause the loop until 1/60th of a second then continue
+            delta = clock.tick(60)
+            # .tick() return delta time in millisecond so we divide by 1000 to get second
+            dt = delta / 1000
+            print(dt)
         screen.fill("black")
         pygame.display.flip()
 
